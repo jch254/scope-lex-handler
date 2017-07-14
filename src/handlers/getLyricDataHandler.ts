44155ba2664ecf06d92fe2ddea1f88d60941c05f
Jordan Hornblow <jordan@jch254.com>
2017-07-14 18:54:41 +1000

    Continue getLyricData execution if audio features are not found

--- src/handlers/getLyricDataHandler.ts
+++ src/handlers/getLyricDataHandler.ts
@@ -13,6 +13,79 @@ export interface GetLyricDataIntent {
   slots: GetLyricDataSlots;
 }
 
+const getResponseMessage = (fullGeniusSong: any, audioFeatures?: any) => {
+  const samples = fullGeniusSong.song_relationships.find((r: any) => r.type === 'samples').songs;
+  const sampledIn = fullGeniusSong.song_relationships.find((r: any) => r.type === 'sampled_in').songs;
+
+  let responseMessage = `Title: ${fullGeniusSong.title}
+Artist: ${fullGeniusSong.primary_artist.name}`;
+
+  if (fullGeniusSong.album !== null) {
+    responseMessage += `
+Album: ${fullGeniusSong.album.name}`;
+  }
+
+  if (fullGeniusSong.release_date !== null) {
+    responseMessage += `
+Release date: ${fullGeniusSong.release_date}`;
+  }
+
+  if (fullGeniusSong.recording_location !== null) {
+    responseMessage += `
+Recorded at: ${fullGeniusSong.recording_location}`;
+  }
+
+  if (audioFeatures !== undefined) {
+    responseMessage += `
+BPM: ${audioFeatures.tempo.toFixed(0)}
+Key: ${mapPitchClassToKey(audioFeatures.key)} ${mapMode(audioFeatures.mode)}`;
+  }
+
+  if (samples.length > 0) {
+    responseMessage += `
+
+Sample${samples.length === 1 ? '' : 's'}
+${samples.map((s: any) => `- ${s.full_title}`).join('\n')}`;
+  }
+
+  if (sampledIn.length > 0) {
+    responseMessage += `
+
+Sampled in
+${sampledIn.map((s: any) => `- ${s.full_title}`).join('\n')}`;
+  }
+
+  if (fullGeniusSong.featured_artists.length > 0) {
+    responseMessage += `
+
+Featuring
+${fullGeniusSong.featured_artists.map((f: any) => `- ${f.name}`).join('\n')}`;
+  }
+
+  if (fullGeniusSong.producer_artists.length > 0) {
+    responseMessage += `
+
+Producer${fullGeniusSong.producer_artists.length === 1 ? '' : 's'}
+${fullGeniusSong.producer_artists.map((p: any) => `- ${p.name}`).join('\n')}`;
+  }
+
+  if (fullGeniusSong.writer_artists.length > 0) {
+    responseMessage += `
+
+Writer${fullGeniusSong.writer_artists.length === 1 ? '' : 's'}
+${fullGeniusSong.writer_artists.map((w: any) => `- ${w.name}`).join('\n')}`;
+  }
+
+  return responseMessage;
+};
+
+const getAttachment = (fullGeniusSong: any, url: string) => ({
+  title: fullGeniusSong.title_with_featured.substring(0, 80),
+  subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
+  imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
+  attachmentLinkUrl: url,
+});
+
 export default async function getLyricDataHandler(
   event: LexEvent,
   userProfile: UserProfile,
@@ -74,142 +147,59 @@ Try scope another lyric or title.`,
   const soundcloudMedia = fullGeniusSong.media.find((media: any) => media.provider.toLowerCase() === 'soundcloud');
   const appleMusicMedia = fullGeniusSong.media.find((media: any) => media.provider.toLowerCase() === 'apple_music');
 
-  const samples = fullGeniusSong.song_relationships.find((r: any) => r.type === 'samples').songs;
-  const sampledIn = fullGeniusSong.song_relationships.find((r: any) => r.type === 'sampled_in').songs;
-
   let audioFeatures;
-  if (spotifyMedia !== undefined) {
-    const spotifyNativeUriParts = spotifyMedia.native_uri.split(':');
-    const spotifyTrackId = spotifyNativeUriParts[spotifyNativeUriParts.length - 1].trim();
 
-    const audioFeaturesResponse = await spotifyApi.getAudioFeaturesForTrack(spotifyTrackId);
-    audioFeatures = audioFeaturesResponse.body;
-  } else {
+  try {
+    if (spotifyMedia !== undefined) {
+      const spotifyNativeUriParts = spotifyMedia.native_uri.split(':');
+      const spotifyTrackId = spotifyNativeUriParts[spotifyNativeUriParts.length - 1].trim();
 
-    const spotifyTracks = await spotifyApi.searchTracks(
+      const audioFeaturesResponse = await spotifyApi.getAudioFeaturesForTrack(spotifyTrackId);
+      audioFeatures = audioFeaturesResponse.body;
+    } else {
+      const spotifyTracks = await spotifyApi.searchTracks(
 `track:${fullGeniusSong.title.trim()} \
 artist:${fullGeniusSong.primary_artist.name.trim()}\
 ${fullGeniusSong.album !== null ? ` album:${fullGeniusSong.album.name.trim()}` : ''} \
 ${fullGeniusSong.release_date !== null ? ` year:${fullGeniusSong.release_date.substring(0, 4)}` : ''}`,
 { limit : 1 });
 
-    console.log(`spotifyTracks: ${JSON.stringify(spotifyTracks)}`);
+      console.log(`spotifyTracks: ${JSON.stringify(spotifyTracks)}`);
 
-    if (spotifyTracks.body.tracks.total > 0) {
-      const spotifyTrack = spotifyTracks.body.tracks.items[0];
+      if (spotifyTracks.body.tracks.total > 0) {
+        const spotifyTrack = spotifyTracks.body.tracks.items[0];
 
-      spotifyMedia = { url: spotifyTrack.external_urls.spotify };
+        spotifyMedia = { url: spotifyTrack.external_urls.spotify };
 
-      const audioFeaturesResponse = await spotifyApi.getAudioFeaturesForTrack(spotifyTrack.id);
-      audioFeatures = audioFeaturesResponse.body;
-
-      console.log(`audioFeatures: ${JSON.stringify(audioFeatures)}`);
+        const audioFeaturesResponse = await spotifyApi.getAudioFeaturesForTrack(spotifyTrack.id);
+        audioFeatures = audioFeaturesResponse.body;
+      }
     }
+  } catch (err) {
+    console.log(err);
   }
 
-  let responseMessage = `Title: ${fullGeniusSong.title}
-Artist: ${fullGeniusSong.primary_artist.name}`;
-
-  if (fullGeniusSong.album !== null) {
-    responseMessage += `
-Album: ${fullGeniusSong.album.name}`;
-  }
-
-  if (fullGeniusSong.release_date !== null) {
-    responseMessage += `
-Release date: ${fullGeniusSong.release_date}`;
-  }
-
-  if (fullGeniusSong.recording_location !== null) {
-    responseMessage += `
-Recorded at: ${fullGeniusSong.recording_location}`;
-  }
-
-  if (audioFeatures !== undefined) {
-    responseMessage += `
-BPM: ${audioFeatures.tempo.toFixed(0)}
-Key: ${mapPitchClassToKey(audioFeatures.key)} ${mapMode(audioFeatures.mode)}`;
-  }
-
-  if (samples.length > 0) {
-    responseMessage += `
-
-Sample${samples.length === 1 ? '' : 's'}
-${samples.map((s: any) => `- ${s.full_title}`).join('\n')}`;
-  }
-
-  if (sampledIn.length > 0) {
-    responseMessage += `
-
-Sampled in
-${sampledIn.map((s: any) => `- ${s.full_title}`).join('\n')}`;
-  }
-
-  if (fullGeniusSong.featured_artists.length > 0) {
-    responseMessage += `
-
-Featuring
-${fullGeniusSong.featured_artists.map((f: any) => `- ${f.name}`).join('\n')}`;
-  }
-
-  if (fullGeniusSong.producer_artists.length > 0) {
-    responseMessage += `
-
-Producer${fullGeniusSong.producer_artists.length === 1 ? '' : 's'}
-${fullGeniusSong.producer_artists.map((p: any) => `- ${p.name}`).join('\n')}`;
-  }
-
-  if (fullGeniusSong.writer_artists.length > 0) {
-    responseMessage += `
-
-Writer${fullGeniusSong.writer_artists.length === 1 ? '' : 's'}
-${fullGeniusSong.writer_artists.map((w: any) => `- ${w.name}`).join('\n')}`;
-  }
+  console.log(`audioFeatures: ${JSON.stringify(audioFeatures)}`);
 
   const attachments = [];
 
   if (spotifyMedia !== undefined) {
-    attachments.push({
-      title: fullGeniusSong.title_with_featured.substring(0, 80),
-      subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
-      imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
-      attachmentLinkUrl: spotifyMedia.url,
-    });
+    attachments.push(getAttachment(fullGeniusSong, spotifyMedia.url));
   }
 
   if (appleMusicMedia !== undefined) {
-    attachments.push({
-      title: fullGeniusSong.title_with_featured.substring(0, 80),
-      subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
-      imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
-      attachmentLinkUrl: appleMusicMedia.url,
-    });
+    attachments.push(getAttachment(fullGeniusSong, appleMusicMedia.url));
   }
 
   if (soundcloudMedia !== undefined) {
-    attachments.push({
-      title: fullGeniusSong.title_with_featured.substring(0, 80),
-      subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
-      imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
-      attachmentLinkUrl: soundcloudMedia.url,
-    });
+    attachments.push(getAttachment(fullGeniusSong, soundcloudMedia.url));
   }
 
   if (youtubeMedia !== undefined) {
-    attachments.push({
-      title: fullGeniusSong.title_with_featured.substring(0, 80),
-      subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
-      imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
-      attachmentLinkUrl: youtubeMedia.url,
-    });
+    attachments.push(getAttachment(fullGeniusSong, youtubeMedia.url));
   }
 
-  attachments.push({
-    title: fullGeniusSong.title_with_featured.substring(0, 80),
-    subTitle: fullGeniusSong.primary_artist.name.substring(0, 80),
-    imageUrl: fullGeniusSong.song_art_image_thumbnail_url,
-    attachmentLinkUrl: fullGeniusSong.url,
-  });
+  attachments.push(getAttachment(fullGeniusSong, fullGeniusSong.url));
 
   const currentGeniusMatches = intent.slots.geniusSongId === null ?
      geniusMatches
@@ -232,7 +222,7 @@ ${fullGeniusSong.writer_artists.map((w: any) => `- ${w.name}`).join('\n')}`;
       fulfillmentState: 'Fulfilled',
       message: {
         contentType: 'PlainText',
-        content: responseMessage,
+        content: getResponseMessage(fullGeniusSong, audioFeatures),
       },
       responseCard: {
         contentType: 'application/vnd.amazonaws.card.generic',
